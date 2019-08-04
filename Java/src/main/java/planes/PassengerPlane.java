89bc1ecb0d2fb8ab5ec107c68a1d2e7f60562c01
Iryna Nikalayeva <iryna_nikalayeva@epam.com>
2019-08-04 21:49:19 +0300

    rename useless comments from PassengerPlane class

--- Java/src/main/java/planes/PassengerPlane.java
+++ Java/src/main/java/planes/PassengerPlane.java
@@ -4,17 +4,12 @@ import java.util.Objects;
 
 public class PassengerPlane extends Plane{
 
-    //=================FIELDS=================
     private int passengersCapacity;
-
-    //=================CONSTRUCTORS=================
     public PassengerPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, int passengersCapacity) {
         super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
         this.passengersCapacity = passengersCapacity;
     }
 
-
-    //=================METHODS=================
     public int getPassengersCapacity() {
         return passengersCapacity;
     }
@@ -25,14 +20,7 @@ public class PassengerPlane extends Plane{
                 ", passengersCapacity=" + passengersCapacity +
                 '}');
     }
-
-//    @Override
-//    public String toString() {
-//        return super.toString().replace("}",
-//                ", passengersCapacity=" + passengersCapacity +
-//                        '}');
-//    }
-
+    
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
