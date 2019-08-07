a82ca7030b200e02bea25ad51c228a96ac15317b
Iryna Nikalayeva <iryna.nikalayeva@gmail.com>
2019-08-07 16:26:39 +0300

    spaces formatting in Planes classes

--- Java/src/main/java/planes/MilitaryPlane.java
+++ Java/src/main/java/planes/MilitaryPlane.java
@@ -4,7 +4,7 @@ import models.MilitaryType;
 
 import java.util.Objects;
 
-public class MilitaryPlane extends Plane{
+public class MilitaryPlane extends Plane {
 
     private MilitaryType militaryType;
 
