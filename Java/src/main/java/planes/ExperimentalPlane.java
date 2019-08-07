326045afb0d6ed210dc034275da6a1805a512d3c
Iryna Nikalayeva <iryna.nikalayeva@gmail.com>
2019-08-07 16:24:06 +0300

    constructor parameters list formstting in ExperimentalPlanes

--- Java/src/main/java/planes/ExperimentalPlane.java
+++ Java/src/main/java/planes/ExperimentalPlane.java
@@ -3,22 +3,23 @@ package planes;
 import models.ClassificationLevel;
 import models.ExperimentalTypes;
 
-public class ExperimentalPlane extends Plane{
+public class ExperimentalPlane extends Plane {
 
     private ExperimentalTypes experimentalType;
     private ClassificationLevel classificationLevel;
 
-    public ExperimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, ExperimentalTypes experimentalType, ClassificationLevel classificationLevel) {
+    public ExperimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, ExperimentalTypes experimentalType,
+                             ClassificationLevel classificationLevel) {
         super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
         this.experimentalType = experimentalType;
         this.classificationLevel = classificationLevel;
     }
 
-    public ClassificationLevel getClassificationLevel(){
+    public ClassificationLevel getClassificationLevel() {
         return classificationLevel;
     }
 
-    public void setClassificationLevel(ClassificationLevel classificationLevel){
+    public void setClassificationLevel(ClassificationLevel classificationLevel) {
         this.classificationLevel = classificationLevel;
     }
 
